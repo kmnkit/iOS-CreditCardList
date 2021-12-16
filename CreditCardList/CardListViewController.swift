ad4eec68638eb312b5c53762bb1e425c72fb74bb
Marco Kang <kmnkit@gmail.com>
2021-12-16 21:27:50 +0900

    Firebase database

---
+++ CreditCardList/CardListViewController.swift
@@ -0,0 +1,74 @@
+//
+//  CardListViewController.swift
+//  CreditCardList
+//
+//  Created by Sungwoong Kang on 2021/12/16.
+//
+
+import UIKit
+import Kingfisher
+import FirebaseDatabase
+
+class CardListViewController: UITableViewController {
+    // UITableView를 구성하기 위해 필요한 Delegate와 Datasource를 기본 제공된 채 제공하므로 별도 선언 하지 않아도 됨
+    // rootview로 UITableView를 가짐
+    var ref: DatabaseReference!
+    
+    var creditCardList: [CreditCard] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // UITableView Cell Register
+        let nibName = UINib(nibName: "CardListCell", bundle: nil)
+        tableView.register(nibName, forCellReuseIdentifier: "CardListCell")
+        
+        ref = Database.database().reference()
+        
+        ref.observe(.value){[weak self] snapshot in
+            guard let value = snapshot.value as? [String: [String: Any]] else { return }
+            
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: value)
+                let cardData = try JSONDecoder().decode([String: CreditCard].self, from: jsonData)
+                let cardList = Array(cardData.values)
+                self?.creditCardList = cardList.sorted { $0.rank < $1.rank }
+                
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+                
+            } catch let error {
+                print("ERROR JSON Parsing \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return creditCardList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CardListCell", for: indexPath) as? CardListCell else { return UITableViewCell()}
+        
+        cell.rankLabel.text = "\(creditCardList[indexPath.row].rank)위"
+        cell.promotionLabel.text = "\(creditCardList[indexPath.row].promotionDetail.amount)만원 증정"
+        cell.cardNameLabel.text = "\(creditCardList[indexPath.row].name)"
+        
+        let imageURL = URL(string: creditCardList[indexPath.row].cardImageURL)
+        cell.cardImageView.kf.setImage(with: imageURL)
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 상세 화면 전달
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        guard let detailViewController = storyboard.instantiateViewController(identifier: "CardDetailViewController") as? CardDetailViewController else { return }
+        
+        detailViewController.promotionDetail = creditCardList[indexPath.row].promotionDetail
+        self.show(detailViewController, sender: nil)
+    }
+}
